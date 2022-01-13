35af45df50270f1565d731afd7dd84b941ebe0b4
Dani1324 <danieleronchini2@gmail.com>
2022-01-13 18:50:57 +0100

    Upload del sito

---
+++ main.js
@@ -0,0 +1,15 @@
+// Lazy loading
+$( document ).ready(function() {
+   [].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
+         img.setAttribute('src', img.getAttribute('data-src'));
+         img.onload = function() {
+            img.removeAttribute('data-src');
+         };
+   });
+});
+
+// AOS animation library
+AOS.init({
+  offset: 300,
+  duration: 1100,
+});
